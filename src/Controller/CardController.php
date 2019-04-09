43f7f0686f4f0ed4bf1de53615dcadad2a4c0709
Julien Bras <julienbras@sidoine.org>
2019-04-08 23:22:00 -0400

    add download card as pdf option

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\ClassifiedCard;
 
+// Include Dompdf required namespaces
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
 class CardController extends AbstractController
 {
     /**
@@ -21,5 +25,34 @@ class CardController extends AbstractController
             'controller_name' => 'CardController',
             'cards' => $cards,
         ]);
+    }
+
+    /**
+     * @Route("/card/{id}/download", name="card_download")
+     */
+    public function download(ClassifiedCard $card) {
+      $html = $this->renderView('card/print.html.twig', [
+          'card' => $card,
+      ]);
+
+
+      $pdfOptions = new Options();
+
+      // Instantiate Dompdf with our options
+      $dompdf = new Dompdf($pdfOptions);
+
+      // Load HTML to Dompdf
+      $dompdf->loadHtml($html);
+
+      // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+      $dompdf->setPaper('A4', 'portrait');
+
+      // Render the HTML as PDF
+      $dompdf->render();
+
+      // Output the generated PDF to Browser (inline view)
+      $dompdf->stream("card.pdf", [
+          "Attachment" => false
+      ]);
     }
 }
