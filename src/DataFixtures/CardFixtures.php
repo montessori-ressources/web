8fb1c1d1a013fbfb7ad428abb0b4f771577620c7
Julien Bras <julienbras@sidoine.org>
2019-04-10 21:51:57 -0400

    test flysystem

--- src/DataFixtures/CardFixtures.php
+++ src/DataFixtures/CardFixtures.php
@@ -5,29 +5,28 @@ use App\Entity\ClassifiedCard;
 use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesser;
 use Faker;
 
 class CardFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-      $path = 'public/uploads/images';
-      if (!file_exists($path)) {
-          mkdir($path, 0777, true);
-      }
       $faker = Faker\Factory::create('fr_FR');
-        // $product = new Product();
-        // $manager->persist($product);
-
         for($c=0; $c<1; $c++) {
           $card = new ClassifiedCard();
           $image_count = $faker->numberBetween(4,9);
           for($i=0; $i<$image_count; $i++) {
             $image = new Image();
-            $file = $faker->image($path, 400, 400, null, false);
+            $filename = $faker->image(sys_get_temp_dir(), 400, 400, null);
+            $mimetype = MimeTypeGuesser::getInstance()->guess($filename);
+            $size     = filesize($filename);
+            $file = new UploadedFile($filename, basename($filename), $mimetype, $size, null, true);
+
             $label = $faker->word;
             $image->setLabel($label);
-            $image->setName($file);
+            $image->setFile($file);
             $card->addImage($image);
           }
           $manager->persist($card);
