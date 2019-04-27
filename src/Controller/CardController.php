dd1d1d6e603a3e228912de1311d3db6f88b89485
Julien Bras <julienbras@sidoine.org>
2019-04-27 11:44:31 -0400

    fix pdf with new card / nomenclature

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Card;
-use App\Entity\ClassifiedCard;
 use App\Entity\Nomenclature;
 use App\Form\CardType;
 use App\Form\NomenclatureType;
@@ -42,81 +41,63 @@ class CardController extends AbstractController
      * @Route("/nomenclature/upload", name="nomenclature_upload")
      */
     public function uploadNomenclature(Request $request) {
-
         $nomenclature = new Nomenclature();
         $card = new Card();
-        //$card->setNomenclature($nomenclature);
         $nomenclature->addCard($card);
         $form = $this->createForm(NomenclatureType::Class,$nomenclature);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
             $nomenclature = $form->getData();
-
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($nomenclature);
             $entityManager->flush();
-
             return $this->render('card/upload-success.html.twig');
         }
 
         return $this->render('card/upload.html.twig', [
             'formNomenclature' => $form->createView(),
         ]);
-
     }
-
 
     /**
      * @Route("/card/upload", name="card_upload")
      */
     public function uploadCard(Request $request) {
-
         $card = new Card();
         $form = $this->createForm(CardType::Class,$card);
-
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
             $card = $form->getData();
-
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($card);
             $entityManager->flush();
-
             return $this->render('card/upload-success.html.twig');
         }
 
         return $this->render('card/upload.html.twig', [
             'formNomenclature' => $form->createView(),
         ]);
-
     }
-
-
 
     /**
      * @Route("/card/{id}/download2", name="card_download2")
      */
-    public function download2(ClassifiedCard $card) {
+    public function download2(Nomenclature $nomenclature) {
       return $this->render('card/print.html.twig', [
-          'card' => $card,
+          'nomenclature' => $nomenclature,
       ]);
     }
 
     /**
      * @Route("/card/{id}/download", name="card_download")
      */
-    public function download(ClassifiedCard $card, PDFService $service) {
+    public function download(Nomenclature $nomenclature, PDFService $service) {
       $html = $this->renderView('card/print.html.twig', [
-          'card' => $card,
+          'nomenclature' => $nomenclature,
       ]);
-      phpinfo();
-      //$bla = fopen('http://localhost:8000/card/image/74','rb');
-      return new Response("error " );
       // try{
       //   return new PDFResponse($service->generatePdf($html));
       // } catch (\Mpdf\MpdfException $e) { // Note: safer fully qualified exception name used for catch
@@ -130,25 +111,25 @@ class CardController extends AbstractController
       // ["Attachment" => false]);
       // $response->send();
 
-      // $pdfOptions = new Options();
-      // $pdfOptions->set('isRemoteEnabled', true);
+      $pdfOptions = new Options();
+      //$pdfOptions->set('isRemoteEnabled', true);
       //
-      // // Instantiate Dompdf with our options
-      // $dompdf = new Dompdf($pdfOptions);
+      // Instantiate Dompdf with our options
+      $dompdf = new Dompdf($pdfOptions);
+
+      // Load HTML to Dompdf
+      $dompdf->loadHtml($html);
+
+      // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+      $dompdf->setPaper('A4', 'portrait');
+
+      // Render the HTML as PDF
+      $dompdf->render();
       //
-      // // Load HTML to Dompdf
-      // $dompdf->loadHtml($html);
-      //
-      // // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
-      // $dompdf->setPaper('A4', 'portrait');
-      //
-      // // Render the HTML as PDF
-      // $dompdf->render();
-      //
-      // // Output the generated PDF to Browser (inline view)
-      // $dompdf->stream("card.pdf", [
-      //     "Attachment" => false
-      // ]);
+      // Output the generated PDF to Browser (inline view)
+      $dompdf->stream("card.pdf", [
+          "Attachment" => false
+      ]);
     }
 
     /**
