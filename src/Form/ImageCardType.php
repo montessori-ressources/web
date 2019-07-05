f94586517db3ea825fc454156c26628265c0d23a
Julien Bras <julienbras@sidoine.org>
2019-07-05 14:11:12 -0400

    show image on edit screen by using Form Extension (https://symfony.com/doc/current/form/create_form_type_extension.html)

--- src/Form/ImageCardType.php
+++ src/Form/ImageCardType.php
@@ -4,8 +4,8 @@ namespace App\Form;
 
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-//use Symfony\Component\Form\Extension\Core\Type\FileType;
+//use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,8 +15,8 @@ class ImageCardType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            //->add('file', FileType::class)
-            ->add('file', HiddenType::class)
+            ->add('file', FileType::class, ['is_image' => true])
+            //->add('file', HiddenType::class)
         ;
     }
 
