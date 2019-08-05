11d165f5f417ca19245784bd4c753024bdc417aa
Julien Bras <julienbras@sidoine.org>
2019-08-04 22:11:24 -0400

    update to prod delivery script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
+# This script is used to deploy the product to the production environment.
+# See the .travis.yml file for more info.
 #!/bin/sh
 eval "$(ssh-agent -s)"
 chmod 600 ./deploy_key
 ssh-add ./deploy_key
 ssh -T -i ./deploy_key ressourcesmont@montessori-ressources.net <<EOF
-  pwd
   cd web
   git checkout feature/deploy-prod
   git pull
-  print Use PHP7.3
+  echo Use PHP7.3
   alias php='/opt/cpanel/ea-php73/root/usr/bin/php'
 
-  print install packages
-  php composer install # failing because memory_limit=32M
+  echo install packages
+  # Now support team have fixed the memory to 512M it work !
+  php /opt/cpanel/composer/bin/composer install
 
 EOF
